b6678e011ed4337b2cceccb298dd5f24a946d529
vinodkumarDV <dvvinod143@gmail.com>
2022-01-20 22:08:22 +0530

    completed array 1a

---
+++ array-1a.js
@@ -0,0 +1,21 @@
+// 1. A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destruction the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable.
+//    ```js
+    //  const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+    //  console.log(name, skills, scores)
+    //  console.log(jsScore, reactScore)
+
+//    ```
+
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]]
+const [username, skills, scores] = student;
+console.log(username, skills, scores);
+//console.log(name, skills, scores)
+
+
+//console.log(jsScore, reactScore)
+
+
+
+			
+
+
